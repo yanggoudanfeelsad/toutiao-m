397a291fdd7cb52fc94f537048fe6c6537d600f3
“yanggoudanfeelsad” <13772179691@163.com>
2023-11-09 22:31:41 +0800

    添加Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-yuedu1"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
 // 加载全局样式
 import '@/styles/index.less'
+
+import { Button } from 'vant'
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
